b5ed2de974c23c0dc0cdba5b8f171389cf6812e0
Jim Drey <moof@moofit.com>
2012-02-04 14:02:20 -0500

    Initial commit

---
+++ src/com/MoofIT/Minecraft/Anima/Anima.java
@@ -0,0 +1,14 @@
+package com.MoofIT.Minecraft.Anima;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Anima extends JavaPlugin {
+
+	public void onDisable() {
+		
+	}
+
+	public void onEnable() {
+		
+	}
+}
